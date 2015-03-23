52dbb623022c5292f6acfed5ab20ba786bdd805d
Diego Santos <diego.net10@hotmail.com>
2015-03-23 15:36:27 -0500

    Active Admin working with PaperClip

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Product do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :list, :of, :attributes, :on, :model, :category_id, :name, :description, :description_long, :price, :stock_quantity
+  permit_params :list, :of, :attributes, :on, :model, :category_id, :name, :description, :description_long, :price, :stock_quantity, :image
   #
   # or
   #
@@ -13,6 +13,17 @@ ActiveAdmin.register Product do
   #   permitted << :other if resource.something?
   #   permitted
   # end
-
+  form multipart: true do |f|
+    f.inputs "Project Details" do
+      f.input :category_id
+      f.input :name
+      f.input :description
+      f.input :description_long
+      f.input :price
+      f.input :stock_quantity
+      f.input :image, required: false
+    end
+    f.actions
+  end
 
 end
